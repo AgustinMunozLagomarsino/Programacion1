1825edd9a58bf732227d9cc791f8022a76d1796d
AgustinMunozLagomarsino <munozlagomarsinoagustin@gmail.com>
2024-05-07 09:55:41 -0300

    Mi mensaje

---
+++ Notebook/ejercicios.py
@@ -0,0 +1,13 @@
+# Temperatura en Celsius
+temperatura_celsius = 70
+
+# Convertir Celsius a Fahrenheit
+temperatura_fahrenheit = temperatura_celsius * 9/5 + 32
+
+# Convertir Celsius a Kelvin
+temperatura_kelvin = temperatura_celsius + 273.15
+
+# Mostrar resultados
+print("Temperatura en Celsius:", temperatura_celsius)
+print("Temperatura en Fahrenheit:", temperatura_fahrenheit)
+print("Temperatura en Kelvin:", temperatura_kelvin)
