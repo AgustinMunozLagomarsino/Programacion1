4034638508470e52a3a9d5618e11b79e4d43146b
AgustinMunozLagomarsino <munozlagomarsinoagustin@gmail.com>
2024-05-09 10:48:22 -0300

    algo

---
+++ Notebook/tema25Des1.py
@@ -0,0 +1,8 @@
+# aca con el input le pido al usuario que ingrese su nombre 
+nombre = input("Por favor, ingresa tu nombre: ")
+
+# Aca lo mismo que en el primero solo que con la ciudad 
+ciudad_origen = input("Ahora, por favor ingresa tu ciudad de origen: ")
+
+# y aca imprimo el saludo personalizado 
+print("Buenas,", nombre + "Bienvenido a la ciudad de", ciudad_origen +"!" )
