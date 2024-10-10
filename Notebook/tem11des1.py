cb4a456a03d16ffbc7900b535ccd88936c6e0170
AgustinMunozLagomarsino <munozlagomarsinoagustin@gmail.com>
2024-10-10 11:21:23 -0300

    edrftgh

--- Notebook/tem11des1.py
+++ Notebook/tem11des1.py
@@ -1,1 +1,9 @@
-print("Bienvenido al Curso")
+# Definir dos números 
+numero1 = 5
+numero2 = 8
+
+# Sumar los números
+resultado = numero1 + numero2
+
+# Mostrar el resultado del numero
+print(f'La suma de {numero1} y {numero2} es: {resultado}')
