a9ec7861fde3e4fd7dd505afc06b6a94716a688f
AgustinMunozLagomarsino <munozlagomarsinoagustin@gmail.com>
2024-10-10 11:29:26 -0300

    xdcghuj

--- Notebook/tem11des2.py
+++ Notebook/tem11des2.py
@@ -1,3 +1,9 @@
- 
-print("Bienvenido   Agustín") 
-print("al Curso de Programación")
+#Defino la base y altura del rectángulo
+base = 6 
+altura = 14 
+
+#Defino área 
+area = base * altura
+
+#Procemos a decir los resultados 
+print(f"el área de tu rectángulo es: {area}")
