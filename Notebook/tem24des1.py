4030cbd5d9199452e710a93ea145d0693ea3f2df
AgustinMunozLagomarsino <munozlagomarsinoagustin@gmail.com>
2024-11-12 07:43:22 -0300

    sfffss

--- Notebook/tem24des1.py
+++ Notebook/tem24des1.py
@@ -1,13 +1,31 @@
-def ordenar_tres_numeros(a, b, c):
-    if a < b and b < c:
-        return a, b, c
-    elif a < c and c < b:
-        return a, c, b
-    elif b < a and a < c:
-        return b, a, c
-    elif b < c and c < a:
-        return b, c, a
-    elif c < a and a < b:
-        return c, a, b
-    else:
-        return c, b, a
+num1 = 7
+num2 = 5
+num3 = 7
+
+# Inicializar variables para almacenar los números ordenados
+minimo = medio = maximo = None
+
+# Clasificar los números utilizando comparaciones encadenadas
+if num1 < num2 <= num3 or num1 <= num3 <= num2:
+    minimo = num1
+elif num2 <= num1 <= num3 or num2 <= num3 <= num1:
+    minimo = num2
+else:
+    minimo = num3
+
+if num1 <= num2 <= num3 or num3 <= num2 <= num1:
+    medio = num2
+elif num2 <= num1 <= num3 or num3 <= num1 <= num2:
+    medio = num1
+else:
+    medio = num3
+
+#print(num1, ">=", num2, ">=", num3, " el valor es ", num1 >= num2 >= num3)
+if num1 >= num2 >= num3 or num1 >= num3 >= num2:
+    maximo = num1
+elif num2 >= num1 >= num3 or num2 >= num3 >= num1:
+    maximo = num2
+else:
+    maximo = num3
+
+print("Números ordenados:", minimo, medio, maximo)
